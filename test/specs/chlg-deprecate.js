d1b43d7c6f0164a1313e2196f236bfdbdbb3b3a6
Florent Dubois <florent.dubois@devaddict.io>
2016-05-29 17:27:29 +0200

    Add tests for chlg-deprecate

---
+++ test/specs/chlg-deprecate.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var expect     = require('chai').expect;
+var proxyquire = require('proxyquire');
+var sinon      = require('sinon');
+
+var spy = sinon.spy();
+
+var chlgDeprecate = proxyquire('../../lib/chlg-deprecate', {
+  './chlg-insert': spy,
+  './common/sections.js': {
+    DEPRECATED: 'Deprecated'
+  }
+});
+
+describe('chlg-deprecate', function () {
+
+  it('should bind chlgInsert with \'Deprecated\' as section', function () {
+    var message  = 'Hello, friend.';
+    var filename = 'CHANGELOG.md';
+    var callback = function () {};
+
+    chlgDeprecate(message, filename, callback);
+
+    expect(spy.calledWithExactly('Deprecated', message, filename, callback)).to.equal(true);
+  });
+
+});
