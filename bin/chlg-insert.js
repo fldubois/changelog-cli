c9da6598ad775a6a7e8446087069e3ab301e9104
Florent Dubois <florent.dubois@devaddict.io>
2016-06-23 19:50:41 +0200

    Split chlg-insert API and CLI

---
+++ bin/chlg-insert.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var program = require('commander');
+
+var chlgInsert = require('../lib/chlg-insert');
+
+program
+  .usage('[options] <section> <message>')
+  .option('-f, --file [filename]', 'Changelog filename', 'CHANGELOG.md')
+  .parse(process.argv);
+
+if (program.args.length < 2) {
+  console.error('usage: chlg insert [options] <section> <message>');
+  process.exit(1);
+}
+
+chlgInsert(program.args.shift(), program.args.join(' '), program.file, function (err) {
+  if (err) {
+    console.error('chlg-insert: ' + err.message);
+    process.exit(1);
+  }
+});
