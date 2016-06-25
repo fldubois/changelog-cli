fc00eb30ff4d819353a852312da53ed2ff139aba
Florent Dubois <florent.dubois@devaddict.io>
2016-06-25 17:45:11 +0200

    Split chlg-deprecate API and CLI

---
+++ bin/chlg-deprecate.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var chlgDeprecate = require('../lib/chlg-deprecate');
+
+var program = require('commander');
+
+program
+  .usage('[options] <message>')
+  .option('-f, --file [filename]', 'Changelog filename', 'CHANGELOG.md')
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  console.error('usage: chlg deprecate [options] <message>');
+  process.exit(1);
+}
+
+chlgDeprecate(program.args.join(' '), program.file, function (err) {
+  if (err) {
+    console.error('chlg-deprecate: ' + err.message);
+    process.exit(1);
+  }
+});
