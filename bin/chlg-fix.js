2a5bd13f928c9770fd3fb4a2f2a5729c57fc1e38
Florent Dubois <florent.dubois@devaddict.io>
2016-06-26 11:27:45 +0200

    Split chlg-fix API and CLI

---
+++ bin/chlg-fix.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var chlgFix = require('../lib/chlg-fix');
+
+var program = require('commander');
+
+program
+  .usage('[options] <message>')
+  .option('-f, --file [filename]', 'Changelog filename', 'CHANGELOG.md')
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  console.error('usage: chlg fix [options] <message>');
+  process.exit(1);
+}
+
+chlgFix(program.args.join(' '), program.file, function (err) {
+  if (err) {
+    console.error('chlg-fix: ' + err.message);
+    process.exit(1);
+  }
+});
