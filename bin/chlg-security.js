35e6f2dccb297a35a5ddd244007034693202a6a9
Florent Dubois <florent.dubois@devaddict.io>
2016-06-28 21:07:38 +0200

    Split chlg-security API and CLI

---
+++ bin/chlg-security.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var chlgSecurity = require('../lib/chlg-security');
+
+var program = require('commander');
+
+program
+  .usage('[options] <message>')
+  .option('-f, --file [filename]', 'Changelog filename', 'CHANGELOG.md')
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  console.error('usage: chlg security [options] <message>');
+  process.exit(1);
+}
+
+chlgSecurity(program.args.join(' '), program.file, function (err) {
+  if (err) {
+    console.error('chlg-security: ' + err.message);
+    process.exit(1);
+  }
+});
