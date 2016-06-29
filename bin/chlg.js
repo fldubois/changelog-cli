8434ce53cbf20c1694a2f941b3b522a30553ba58
Florent Dubois <florent.dubois@devaddict.io>
2016-06-29 21:04:14 +0200

    Split chlg API and CLI

---
+++ bin/chlg.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var program = require('commander');
+
+program.version(require('../package.json').version)
+  .command('init',      'Initialize a new changelog file')
+  .command('release',   'Set all current changes under a new version')
+  .command('show',      'Show changes')
+  .command('insert',    'Insert a new change in the log file')
+  .command('add',       'Add a new feature in current changes')
+  .command('change',    'Point out a change in an existing functionality')
+  .command('deprecate', 'Tag a feature as deprecated for the upcoming release')
+  .command('remove',    'Point out a feature deleted in the upcoming release')
+  .command('fix',       'Add a bug fix to changes list')
+  .command('security',  'Invite users to upgrade in case of vulnerabilities')
+  .parse(process.argv);
