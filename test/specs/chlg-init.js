e645d845ba6f039af31c9cb2f8f75cfcf2fb5713
Florent Dubois <florent.dubois@devaddict.io>
2016-06-30 22:00:51 +0200

    Fix tests

--- test/specs/chlg-init.js
+++ test/specs/chlg-init.js
@@ -14,6 +14,8 @@ var fixture = path.resolve(__dirname, '../fixtures/CHANGELOG-init.md');
 
 var cwd = '';
 
+var filename = 'CHANGELOG-module.md';
+
 function cleanDataDirectory(done) {
   fs.readdir(dataDir, function (err, files) {
     if (err) {
@@ -49,122 +51,71 @@ describe('chlg-init', function () {
     });
   });
 
-  describe('as a module', function () {
-    var filename = 'CHANGELOG-module.md';
+  before('Delete files in /test/data directory', cleanDataDirectory);
 
-    before('Delete files in /test/data directory', cleanDataDirectory);
+  it('should create a new changelog file', function (done) {
+    chlgInit(filename, function (err) {
+      expect(err).to.not.exist;
 
-    it('should create a new changelog file', function (done) {
-      chlgInit(filename, function (err) {
+      fileCompare(filename, fixture, function (err, match) {
         expect(err).to.not.exist;
-
-        fileCompare(filename, fixture, function (err, match) {
-          expect(err).to.not.exist;
-          expect(match).to.equal(true);
-          return done();
-        });
-      });
-    });
-
-    it('should use \'CHANGELOG.md\' as default filename', function (done) {
-      chlgInit(function (err) {
-        expect(err).to.not.exist;
-
-        fileCompare('CHANGELOG.md', fixture, function (err, match) {
-          expect(err).to.not.exist;
-          expect(match).to.equal(true);
-          return done();
-        });
-      });
-    });
-
-    it('should return error on existing file', function (done) {
-      chlgInit(filename, function (err) {
-        expect(err).to.exist;
-        expect(err.message).to.equal('Cannot create file ‘' + filename + '’: File exists');
-
+        expect(match).to.equal(true);
         return done();
       });
     });
-
-    it('should return error on bad \'file\' parameter type', function (done) {
-      chlgInit(1, function (err) {
-        expect(err).to.exist;
-        expect(err.message).to.equal('Parameter ‘file’ must be a string');
-
-        return done();
-      });
-    });
-
-    it('should return file writing error', function (done) {
-      // Fix the 'possible EventEmitter memory leak detected' warning
-      require('events').defaultMaxListeners = 15;
-
-      var chlgInit = proxyquire('../../lib/chlg-init', {
-        'fs': {
-          writeFile: function (file, data, options, callback) {
-            return callback(new Error('Fake error'));
-          }
-        }
-      });
-
-      chlgInit('CHANGELOG-error.md', function (err) {
-        expect(err).to.exist;
-        expect(err.message).to.equal('Cannot create file ‘CHANGELOG-error.md’: Fake error');
-
-        return done();
-      });
-    });
-
-    after('Delete files in /test/data directory', cleanDataDirectory);
-
   });
 
-  describe('as a command', function () {
-    before('Delete files in /test/data directory', cleanDataDirectory);
-
-    it('should create a new changelog file with default name', function (done) {
-      var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg-init.js')], {timeout: 1500});
-
-      expect(child.status).to.equal(0);
-      expect(child.stdout.toString()).to.equal('');
-      expect(child.stderr.toString()).to.equal('');
+  it('should use \'CHANGELOG.md\' as default filename', function (done) {
+    chlgInit(function (err) {
+      expect(err).to.not.exist;
 
       fileCompare('CHANGELOG.md', fixture, function (err, match) {
         expect(err).to.not.exist;
         expect(match).to.equal(true);
-        done();
-      });
-
-    });
-
-    it('should create a new changelog file with custom name', function (done) {
-      var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg-init.js'), '-f', 'CHANGELOG-custom.md'], {timeout: 1500});
-
-      expect(child.status).to.equal(0);
-      expect(child.stdout.toString()).to.equal('');
-      expect(child.stderr.toString()).to.equal('');
-
-      fileCompare('CHANGELOG-custom.md', fixture, function (err, match) {
-        expect(err).to.not.exist;
-        expect(match).to.equal(true);
-        done();
+        return done();
       });
     });
+  });
 
-    it('should exit with error status on existing file', function (done) {
-      var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg-init.js')], {timeout: 1500});
-
-      expect(child.status).to.equal(1);
-      expect(child.stdout.toString()).to.equal('');
-      expect(child.stderr.toString()).to.equal('chlg-init: Cannot create file ‘CHANGELOG.md’: File exists\n');
+  it('should return error on existing file', function (done) {
+    chlgInit(filename, function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal('Cannot create file ‘' + filename + '’: File exists');
 
       return done();
     });
-
-    after('Delete files in /test/data directory', cleanDataDirectory);
-
   });
+
+  it('should return error on bad \'file\' parameter type', function (done) {
+    chlgInit(1, function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal('Parameter ‘file’ must be a string');
+
+      return done();
+    });
+  });
+
+  it('should return file writing error', function (done) {
+    // Fix the 'possible EventEmitter memory leak detected' warning
+    require('events').defaultMaxListeners = 15;
+
+    var chlgInit = proxyquire('../../lib/chlg-init', {
+      'fs': {
+        writeFile: function (file, data, options, callback) {
+          return callback(new Error('Fake error'));
+        }
+      }
+    });
+
+    chlgInit('CHANGELOG-error.md', function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal('Cannot create file ‘CHANGELOG-error.md’: Fake error');
+
+      return done();
+    });
+  });
+
+  after('Delete files in /test/data directory', cleanDataDirectory);
 
   after('Restore CWD', function () {
     process.chdir(cwd);
--- test/specs/chlg.js
+++ test/specs/chlg.js
@@ -22,41 +22,10 @@ var commands = [
 
 describe('chlg', function() {
 
-  describe('as a module', function() {
-
-    commands.forEach(function (command) {
-      it('should export chlg-' + command, function () {
-        expect(chlg[command]).to.be.a('function');
-      });
+  commands.forEach(function (command) {
+    it('should export chlg-' + command, function () {
+      expect(chlg[command]).to.be.a('function');
     });
-
-  });
-
-  describe('as a command', function() {
-
-    it('should show the chlg version', function () {
-      var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg.js'), 'version'], {timeout: 1500});
-
-      expect(child.status).to.equal(0);
-      expect(child.stdout.toString().replace('\n', '')).to.equal(require('../../package.json').version);
-    });
-
-    it('should show the help message', function () {
-      var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg.js'), 'help'], {timeout: 1500});
-
-      expect(child.status).to.equal(0);
-      expect(child.stdout.toString()).to.have.string('Usage: chlg [options] [command]');
-    });
-
-    commands.forEach(function (command) {
-      it('should support the command ' + command, function () {
-        var child = spawnSync(process.execPath, [path.resolve(__dirname, '../../lib/chlg.js'), 'help', command], {timeout: 1500});
-
-        expect(child.status).to.equal(0);
-        expect(child.stdout.toString()).to.have.string('Usage: chlg-' + command);
-      });
-    });
-
   });
 
 });
