dffce56f3f6485361c8b603a58fbddad796bee30
Florent Dubois <florent.dubois@devaddict.io>
2016-07-15 22:42:48 +0200

    Add JSDoc in chlg-security

--- test/specs/chlg-security.js
+++ test/specs/chlg-security.js
@@ -15,7 +15,7 @@ var chlgSecurity = proxyquire('../../lib/chlg-security', {
 
 describe('chlg-security', function () {
 
-  it('should bind chlgInsert with \'Security\' as section', function () {
+  it('should call chlgInsert with \'Security\' as section', function () {
     var message  = 'Hello, friend.';
     var filename = 'CHANGELOG.md';
     var callback = function () {};
